aced2e78760f280d2697bf05067daccbc164a3c5
Steven B <stevenbutar51@gmail.com>
2019-11-11 19:53:05 +0700

    update minggu 8

---
+++ latihan_1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Koneksi Database MySQL</title>
+</head>
+<body>
+	<h1>Demo koneksi database MySQL</h1>
+	<?php 
+		$conn = mysqli_connect("localhost","root","");
+		if ($conn) {
+			echo "OK";
+		}else{
+			echo "Server Not Connected";
+		}
+	 ?>
+</body>
+</html>
